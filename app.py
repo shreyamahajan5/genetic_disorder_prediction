636567f3634826854b8abe01f3875d490dc754fb
Shreya Mahajan <108216882+shreyamahajan5@users.noreply.github.com>
2023-11-02 02:26:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import scikit-learn
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
